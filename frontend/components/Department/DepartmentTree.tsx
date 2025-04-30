8732ba357f733d1677e8b40e88601460fc10c32d
HadrielWonda <olawalebash2017@gmail.com>
2025-04-30 21:33:29 +0100

    Fix import paths in DepartmentTree and LoginPage components; ensure React is imported where necessary

--- frontend/components/Department/DepartmentTree.tsx
+++ frontend/components/Department/DepartmentTree.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_DEPARTMENTS } from '../../queries/departments';
 
--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { useMutation } from '@apollo/client';
-import { LOGIN } from '../queries/auth';
+import { LOGIN } from '../graphql/queries/auth'; // Update the path to the correct location
 import { useRouter } from 'next/router';
 
 export default function LoginPage() {
