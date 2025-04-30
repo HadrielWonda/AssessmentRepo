8c3e4d7d63458646bff635f90617c3f219a525f5
HadrielWonda <olawalebash2017@gmail.com>
2025-04-30 23:58:52 +0100

    Update dependencies and enhance TypeScript configuration; add Apollo Client, adjust entity imports, and include jsonwebtoken types

--- src/data-source.ts
+++ src/data-source.ts
@@ -5,12 +5,26 @@ import { User } from './entities/user.entity';
 import { Department } from './entities/department.entity';
 import { SubDepartment } from './entities/sub-department.entity';
 
+
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' ? { 
+    rejectUnauthorized: false 
+  } : false,
   synchronize: true,
   logging: false,
-  entities: [User, Department, SubDepartment],
-  migrations: [],
-  subscribers: [],
+  entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
 });
+
+// export const AppDataSource = new DataSource({
+//   type: 'postgres',
+//   url: process.env.DATABASE_URL,
+//   synchronize: true,
+//   logging: false,
+//   entities: [User, Department, SubDepartment],
+//   migrations: [],
+//   subscribers: [],
+// });
